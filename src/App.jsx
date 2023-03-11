baca5281d48aa1cce570cc9d1a90f074322c271f
Luiz Lima <luiz.lima@squadra.com.br>
2023-03-11 18:52:16 -0300

    refactor: Ajustando código desnecessário

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { ToastContainer } from "react-toastify";
 import { Buttons, MainTable } from "./components";
 
 function App() {
   return (
     <div>
-      <ToastContainer />
       <Buttons />
       <MainTable />
     </div>
