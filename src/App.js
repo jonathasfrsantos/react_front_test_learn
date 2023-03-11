392ee693bfd88e02908a4c5f5ea3fa580036b735
jonathasfrsantos <jonathasfrsantos@gmail.com>
2023-03-11 11:29:56 -0300

    Button puro, sem ações

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Buttons from './components/Buttons/buttons';
 import MainTable from './components/Tables/mainTable';
 
 function App() {
   return( 
     <div>
+        <Buttons />
         <MainTable />
     </div>
     
---
+++ src/components/Buttons/buttons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function Buttons() {
+  return (
+    <Button variant="primary">Adicionar</Button>
+  );
+}
+
+export default Buttons;
