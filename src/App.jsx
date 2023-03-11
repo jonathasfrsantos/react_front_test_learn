2d35cbd308e59278d99853e9aa7430170920d6ba
Luiz Lima <luiz.lima@squadra.com.br>
2023-03-11 18:49:34 -0300

    refactor: Remodulando demais arquivos

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { ToastContainer } from "react-toastify";
+import { Buttons, MainTable } from "./components";
+
+function App() {
+  return (
+    <div>
+      <ToastContainer />
+      <Buttons />
+      <MainTable />
+    </div>
+  );
+}
+
+export default App;
